a916d92f1400a46c234a59e53b3aabe298b55b37
Levi Jones <leviwill.j@gmail.com>
2024-03-16 22:46:10 -0600

    Flies now spawn throughout the round

---
+++ Spawner.gd
@@ -0,0 +1,27 @@
+extends Path2D
+
+@export var enemies = [
+	preload('res://fly_enemy.tscn')
+]
+
+@export var timeSinceLastSpeedup = -6.0
+@export var speedupInterval = 10.0
+@export var timeSinceLastSpawn = -5.0
+@export var spawnInterval = 11.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	timeSinceLastSpawn += delta
+	timeSinceLastSpeedup += delta
+	
+	if (timeSinceLastSpawn > spawnInterval):
+		var enemy = enemies[0].instantiate()
+		add_child(enemy)
+		timeSinceLastSpawn = 0.0
+	
+	if (timeSinceLastSpeedup > speedupInterval):
+		spawnInterval /= 1.2
+		timeSinceLastSpeedup = 0
+		print(spawnInterval)
+		
