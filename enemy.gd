b897868fc75a5e0ad51597cadf06f7b9114d69bf
Levi Jones <leviwill.j@gmail.com>
2024-03-16 19:30:53 -0600

    Bugs dissapear at end of path

--- enemy.gd
+++ enemy.gd
@@ -3,7 +3,17 @@ extends PathFollow2D
 @export var move_speed = 50
 @export var health = 100
 
+var last_frame_pos
+
 func _physics_process(delta):
 	set_progress(get_progress() + move_speed * delta)
-	if (progress_ratio == 100):
-		print("done")
+	
+	if (last_frame_pos):
+		if (last_frame_pos.x > position.x):
+			scale.x = -1
+		else:
+			scale.x = 1
+	last_frame_pos = position
+	
+	if (progress_ratio >= 1): # we finished the path
+		queue_free() # Delete node
