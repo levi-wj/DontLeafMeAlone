bfc2946bf94d09259d89b5e819d8a0401c793a41
nortories <sooaemalelagij@gmail.com>
2024-03-16 19:39:28 -0600

    menubar

---
+++ ToolBar.gd
@@ -0,0 +1,26 @@
+extends TextureButton
+
+@onready var progress_bar = $TextureProgressBar
+@onready var timer = $Timer
+@onready var time = $Time
+
+func _ready():
+	progress_bar.max_value = timer.wait_time
+	set_process(false)
+	
+func _process(_delta):
+	time.text = "%3.1f" % timer.time_left
+	progress_bar.value = timer.time_left
+
+
+func _on_pressed():
+	timer.start()
+	disabled = true
+	set_process(true)
+
+
+func _on_timer_timeout():
+	disabled = false
+	time.text = ""
+	set_process(false)
+	
