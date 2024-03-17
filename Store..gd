06be35ea025678b778a18ff3992a3f2d981a0d2f
nortories <sooaemalelagij@gmail.com>
2024-03-17 13:09:12 -0600

    shopmenutab

---
+++ Store..gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+# Called when the node enters the scene tree for the first time.
+@onready var flower_store = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/FlowerStore
+@onready var fertilizer_store = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/FertilizerStore
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_tab_bar_tab_button_pressed(tab):
+	print(tab)
+
+func _on_tab_bar_tab_changed(tab):
+	if tab == 0:
+		fertilizer_store.visible = false
+		flower_store.visible= true
+	elif tab == 1:
+		flower_store.visible= false
+		fertilizer_store.visible = true
